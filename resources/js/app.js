cc886d5c16b362e8bed08e0c309eca511a835870
karnurmax <karnurmax@mail.ru>
2020-10-10 22:21:11 +0500

    copy from qr-code-generator state

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,34 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue';
+import VueRouter from 'vue-router'
 
-require('./bootstrap');
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-window.Vue = require('vue');
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Vue.use(VueRouter)
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
+import App from './views/App.vue'
+const router = new VueRouter({
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: () =>
+                import ('./views/Home.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () =>
+                import ('./views/Login.vue'),
+        },
+    ],
 });
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
