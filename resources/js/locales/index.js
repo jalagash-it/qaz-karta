87bc2a9f78f1aef5eef0b9bbd03c1bc61a2fa44a
zhusupov92 <zhusupov.92@bk.ru>
2020-12-01 23:46:09 +0500

    Localization languages

--- resources/js/locales/index.js
+++ resources/js/locales/index.js
@@ -2,19 +2,37 @@ export default {
     kk: {
         navbar: {
             guest: "Қонақ",
-            lang: "тіл"
+            lang: "тіл",
+            search: "іздеу",
+            profile: "профиль",
+            Signout: "кіру",
+            user: "қолданушы",
+            login: "логин",
+            register: "тіркеу"
         }
     },
     ru: {
         navbar: {
             guest: "Гость",
-            lang: "язык"
+            lang: "язык",
+            search: "поиск",
+            profile: "профиль",
+            Signout: "войти",
+            user: "пользователь",
+            login: "логин",
+            register: "регистрация"
         }
     },
     en: {
         navbar: {
             guest: "Guest",
-            lang: "lang"
+            lang: "lang",
+            search: "search",
+            profile: "profile",
+            Signout: "Sign out",
+            user: "user",
+            login: "login",
+            register: "register"
         }
     }
 };
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="main-container">
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="#">QAZ-KARTA</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
+<!--
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item href="#">Link</b-nav-item>
           <b-nav-item href="#" disabled>Disabled</b-nav-item>
         </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
+         Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
             <b-form-input
               size="sm"
               class="mr-sm-2"
-              placeholder="Search"
+              :placeholder="$t('navbar.search')"
             ></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit"
-              >Search</b-button
-            >
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">
+              {{ $t("navbar.search") }}
+            </b-button>
           </b-nav-form>
 
           <b-nav-item-dropdown :text="$t('navbar.lang')" right>
@@ -38,18 +38,26 @@
           <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
             <template v-slot:button-content>
-              <em>User</em>
+              <em>{{ $t("navbar.user") }}</em>
             </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="#">
+              {{ $t("navbar.profile") }}
+            </b-dropdown-item>
+            <b-dropdown-item href="#">
+              {{ $t("navbar.Signout") }}
+            </b-dropdown-item>
           </b-nav-item-dropdown>
           <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
             <template v-slot:button-content>
               <em>{{ $t("navbar.guest") }}</em>
             </template>
-            <b-dropdown-item href="#">login</b-dropdown-item>
-            <b-dropdown-item href="#">register</b-dropdown-item>
+            <b-dropdown-item href="#">
+              {{ $t("navbar.login") }}
+            </b-dropdown-item>
+            <b-dropdown-item href="#">
+              {{ $t("navbar.register") }}
+            </b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
